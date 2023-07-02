819217e4150cfc94e3136f4a9ac2b5ed622d2f5a
Omid Tarabavar <omidtarab1300@gmail.com>
2023-07-03 03:59:58 +0430

    Add a bit of architecture of WSystem entity

--- WSystem.vhd
+++ WSystem.vhd
@@ -8,3 +8,17 @@ entity WSystem is
 		MOUT : out std_logic_vector(2 downto 0);
 		SEG : out std_logic_vector(6 downto 0));
 end WSystem;
+
+architecture WS of WSystem is
+type STATE_TYPE is (ST0,ST1);
+signal PS,NS : STATE_TYPE;
+
+CLK_PROC :  process(RESET,NS,CLK)
+begin
+	if (RESET = '1') then PS <= ST0;
+	elsif (rising_edge(CLK) then PS <= NS;
+	end if
+end process;
+
+CMB_PROC : process(T,L,M,PS)
+begin
