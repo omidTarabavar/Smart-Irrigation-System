4313ba729dbd2b82e10baf71f7c0247f908f3ada
Omid Tarabavar <omidtarab1300@gmail.com>
2023-07-02 22:30:07 +0430

    Add entity of WSystem

--- WSystem.vhd
+++ WSystem.vhd
@@ -1,1 +1,10 @@
+library IEEE;
+use IEEE.STD_LOGIC_1164.all;
 
+entity WSystem is
+	port(TIN,LIN,RESET,CLK : in std_logic;
+	    	MIN : in std_logic_vector(2 downto 0);
+		LOUT,TOUT,STATE : out std_logic;
+		MOUT : out std_logic_vector(2 downto 0);
+		SEG : out std_logic_vector(6 downto 0));
+end WSystem;
